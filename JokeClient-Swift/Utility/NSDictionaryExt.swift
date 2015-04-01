ad66fb1dbe20307b264704a96e8097c220e05f87
user <user@admintekiMac-mini.local>
2015-04-01 15:18:10 +0800

    can run

---
+++ JokeClient-Swift/Utility/NSDictionaryExt.swift
@@ -0,0 +1,32 @@
+//
+//  NSDictionary-Null.swift
+//  JokeClient-Swift
+//
+//  Created by YANGReal on 14-6-7.
+//  Copyright (c) 2014年 YANGReal. All rights reserved.
+//
+
+import UIKit
+import Foundation
+extension NSDictionary
+{
+   
+    func stringAttributeForKey(key:String)->String
+    {
+        var obj : AnyObject! = self[key]
+        
+        if obj == nil
+        {
+            return ""
+        }
+        
+        if obj.isKindOfClass(NSNumber)
+        {
+            var num = obj as NSNumber
+            return num.stringValue
+        }
+        
+        return obj as String
+    }
+    
+}
